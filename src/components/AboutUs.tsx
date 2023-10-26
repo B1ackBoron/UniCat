3321bb5a6220cb63ba242f6f5c3655cf6f41c7e4
B1acK_BoroN <galatonovdenis@mail.ru>
2023-10-26 17:38:54 +0300

    added images to about us

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -1,19 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import aboutUs from "../images/aboutImages/aboutUs.svg";
 
 type Props = {};
 
 const AboutUs = (props: Props) => {
   const aboutUsItems = [
-    { label: `About us`, url: `/about-us` },
+    { label: `About us`, url: `/about-us`, img: aboutUs },
     { label: `About breed`, url: `/about-us/breed` },
     { label: `Reviews`, url: `/about-us/reviews` },
     { label: `FAQ`, url: `/about-us/faq` },
   ];
+
   const pathname = usePathname();
   return (
     <div className="absolute top-[98px]">
-      {aboutUsItems.map(({ url, label }, index) => (
+      {aboutUsItems.map(({ url, label, img }, index) => (
         <Link
           key={index}
           href={url}
@@ -21,6 +24,7 @@ const AboutUs = (props: Props) => {
             url === pathname ? "text-white font-semibold" : "text-gray-500"
           }`}
         >
+          <Image src={img} alt={label} />
           {label}
         </Link>
       ))}
